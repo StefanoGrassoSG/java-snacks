9ba72293291158fcfbd82733620c80be136c4e36
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-10 12:11:01 +0100

    snack4 complete

---
+++ src/org/lessons/java/snack4.java
@@ -0,0 +1,22 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class snack4 {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserisci una parola: ");
+		String strValue = in.nextLine();
+		
+		for(int x=0;x<strValue.length()/2;x++) {
+			
+			if(strValue.charAt(x) != strValue.charAt(strValue.length() - 1 - x)) {
+				System.out.println("La parola " + strValue + " non è palindroma");
+	            return;
+			}
+		}
+		 System.out.println("La parola " + strValue + " è palindroma");
+	}
+}
