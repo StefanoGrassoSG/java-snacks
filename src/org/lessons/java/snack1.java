a23d195e7540b996f19be2b71a585899e995a269
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-10 11:07:10 +0100

    snack 1 complete

--- src/org/lessons/java/snack1.java
+++ src/org/lessons/java/snack1.java
@@ -1,5 +1,22 @@
 package org.lessons.java;
 
-public class snack1 {
+import java.util.Scanner;
 
+public class snack1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero: ");
+		String strValue1 = in.nextLine();
+		int value1 = Integer.valueOf(strValue1);
+		
+		if(value1 % 2 == 0) {
+			System.out.println(value1 + " " + "è pari");
+		}
+		else {
+			System.out.println(value1 + " " + "è dispari");
+		}
+	}
 }
