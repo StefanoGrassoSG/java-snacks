c7da1904ac6bc543c880dcf97098bbd6c081d2f3
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-10 15:50:28 +0100

    snack 9 complete

---
+++ src/org/lessons/java/snack9.java
@@ -0,0 +1,23 @@
+package org.lessons.java;
+
+public class snack9 {
+	
+	public static void main(String[] args) {
+		
+		int[] numbers = {1, 2, 3, 4, 5};
+		int [] pairs = new int[10];
+		int [] odd = new int[10];
+		
+		for(int x=0;x<numbers.length;x++) {
+			int	single = numbers[x];
+			if(single % 2 == 0) {
+				pairs[x] = single;
+				System.out.println(pairs[x] + "pari");
+			}
+			else {
+				odd[x] = single;
+				System.out.println(odd[x] + "dispari");
+			}
+		}
+	}
+}
