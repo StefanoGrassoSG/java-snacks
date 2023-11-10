565d5bcb17fd6c003cefae8e227e7a5edbc0a7be
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-10 16:33:32 +0100

    snack 11 complete

---
+++ src/org/lessons/java/snack11.java
@@ -0,0 +1,35 @@
+package org.lessons.java;
+
+import java.util.Random;
+
+public class snack11 {
+	
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		int [] numbers = new int[101];
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+		for(int x=0;x<=100;x++) {
+			int randomNumber = random.nextInt(100);
+			numbers[x] = randomNumber;
+			System.out.println(numbers[x]);
+			
+			if(numbers[x] < min) {
+				min = numbers[x];
+			}
+			else if(numbers[x] > max) {
+				max = numbers[x];
+			}
+			
+			 sum += numbers[x];
+		}
+		
+		double average = (double) sum / 100;
+		System.out.println("il numero più basso è: " + min);
+		System.out.println("il numero più alto è: " + max);
+		System.out.println("la media dei numeri è: " + average);
+	}
+}
