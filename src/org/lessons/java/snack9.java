c2075d25c6701218b96d198861208eba221094f0
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-13 11:20:29 +0100

    refactoring3

--- src/org/lessons/java/snack9.java
+++ src/org/lessons/java/snack9.java
@@ -5,14 +5,14 @@ public class snack9 {
 	public static void main(String[] args) {
 		
 		int[] numbers = {1, 2, 3, 4, 5};
-		int [] pairs = new int[10];
+		int [] evens = new int[10];
 		int [] odd = new int[10];
 		
 		for(int x=0;x<numbers.length;x++) {
 			int	single = numbers[x];
 			if(single % 2 == 0) {
-				pairs[x] = single;
-				System.out.println(pairs[x] + "pari");
+				evens[x] = single;
+				System.out.println(evens[x] + "pari");
 			}
 			else {
 				odd[x] = single;
